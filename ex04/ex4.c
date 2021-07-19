34753bd09df23e53b0ca64ae694f779892483008
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-19 04:37:00 -0400

    Corrections

--- ex04/ex4.c
+++ ex04/ex4.c
@@ -2,10 +2,10 @@
 
 int main(){
 	int age = 10;
-	int height;
+	int height = 5;
 
-	printf("I am %d years old.\n");
-	printf("I am %d inches tall.\n",height);
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
 
 	return 0;
 }
